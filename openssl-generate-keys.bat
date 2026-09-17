e6538a13a3d1cac3c61081360265f7b7da641f63
Yunpeng Li <54851187+yunpengliDataScience@users.noreply.github.com>
2026-09-17 17:41:30 -0400

    Update openssl-generate-keys.bat (Generate cert for Spring Apps)

--- openssl-generate-keys.bat
+++ openssl-generate-keys.bat
@@ -4,17 +4,35 @@ REM Download OpenSSL at:
 REM https://kb.firedaemon.com/support/solutions/articles/4000121705-openssl-binary-distributions-for-microsoft-windows#ZIP-File
 REM -----------------------------------------------------------------------------------------------------------------------------
 
-set OPENSSL_HOME=C:\tools\openssl-4.0.1
+REM set OPENSSL_HOME=C:\tools\openssl-4.0.1
+set OPENSSL_HOME=C:\Projects\SpringBoot_SAML_Keycloak_Demo\openssl-4.0.1
 set OPENSSL_CONF=%OPENSSL_HOME%\ssl\openssl.cnf
 set PATH=%OPENSSL_HOME%\x64\bin;%PATH%
 
 cd .\keycloak-certs
 
-%OPEN_SSL_HOME%\openssl req ^
+REM openssl req ^
+REM   -newkey rsa:2048 ^
+REM   -nodes ^
+REM   -keyout keycloak.key ^
+REM   -x509 ^
+REM   -days 365 ^
+REM   -out keycloak.crt ^
+REM   -subj "/CN=localhost"
+  
+openssl req ^
   -newkey rsa:2048 ^
   -nodes ^
   -keyout keycloak.key ^
   -x509 ^
-  -days 365 ^
+  -days 3650 ^
   -out keycloak.crt ^
-  -subj "/CN=localhost"
+  -subj "/CN=keycloak-dev" ^
+  -addext "subjectAltName = DNS:localhost, IP:127.0.0.1, IP:10.86.44.244"
+
+
+
+REM Convert OpenSSL Certificate to a PKCS12 Keystore for Spring Boot applications
+openssl pkcs12 -export -in keycloak.crt -inkey keycloak.key -out springboot.p12 -name springboot -passout pass:springbootpass
+
+cd ..
